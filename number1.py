21f2dc80e8176d57cce9bee03978f85881d1c531
Fadil Fidrian <f.fidrian@yahoo.com>
2022-12-22 09:06:13 +0700

    perqara test

--- number1.py
+++ number1.py
@@ -6,15 +6,6 @@ import random
 import re
 import sys
 
-#
-# Complete the 'activityNotifications' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER_ARRAY expenditure
-#  2. INTEGER d
-#
-
 def getMedian(countExp, d, mid):
     counter = 0
     position = 0
@@ -58,17 +49,10 @@ def activityNotifications(expenditure, d):
     
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     first_multiple_input = input().rstrip().split()
-
     n = int(first_multiple_input[0])
-
     d = int(first_multiple_input[1])
-
     expenditure = list(map(int, input().rstrip().split()))
-
     result = activityNotifications(expenditure, d)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
--- number2.py
+++ number2.py
@@ -6,20 +6,7 @@ import random
 import re
 import sys
 
-#
-# Complete the 'queensAttack' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER k
-#  3. INTEGER r_q
-#  4. INTEGER c_q
-#  5. 2D_INTEGER_ARRAY obstacles
-#
-
 def queensAttack(n, k, r_q, c_q, obstacles):
-    # no obstacles
     left = c_q-1
     right = n-c_q
     up = n-r_q
@@ -75,26 +62,17 @@ def queensAttack(n, k, r_q, c_q, obstacles):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     first_multiple_input = input().rstrip().split()
-
     n = int(first_multiple_input[0])
-
     k = int(first_multiple_input[1])
-
     second_multiple_input = input().rstrip().split()
-
     r_q = int(second_multiple_input[0])
-
     c_q = int(second_multiple_input[1])
-
     obstacles = []
 
     for _ in range(k):
         obstacles.append(list(map(int, input().rstrip().split())))
 
     result = queensAttack(n, k, r_q, c_q, obstacles)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
